64827a5815300906d9b3765981f4e4f189d12325
Ranjith V Uppuluri <83759451+Ranjith-V-Uppuluri@users.noreply.github.com>
2021-06-18 18:49:36 +0530

    program in C to find the LCM of two numbers using recursion
    OutPut:
    enter two numbers12 30
    LCM of 12 and 30 is 60

---
+++ LCMusingRecursion.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+ 
+// Recursive function to return gcd of a and b
+int gcd(int a, int b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
+ 
+// Function to return LCM of two numbers
+int lcm(int a, int b)
+{
+    return (a / gcd(a, b)) * b;
+}
+ 
+// Driver program to test above function
+int main()
+{
+    int a,b;
+    printf("enter two numbers");
+    scanf("%d %d",&a,&b);
+    printf("LCM of %d and %d is %d ", a, b, lcm(a, b));
+    return 0;
+}
